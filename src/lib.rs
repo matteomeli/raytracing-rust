abf8b583c919abca7c842f3c4322a530059c5b6b
Matteo Meli <matteo.meli@gmail.com>
2019-12-26 13:04:15 +0000

    add vector

---
+++ src/lib.rs
@@ -0,0 +1,189 @@
+use std::fmt;
+use std::ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg};
+
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vec3 {
+    pub fn zero() -> Self {
+        Vec3::new(0.0, 0.0, 0.0)
+    }
+
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Vec3 { x, y, z }
+    }
+
+    pub fn into_unit(v: Vec3) -> Self {
+        let length = v.length();
+        v / length
+    }
+
+    // As color
+    pub fn r(&self) -> f32 {
+        self.x
+    }
+    pub fn g(&self) -> f32 {
+        self.y
+    }
+    pub fn b(&self) -> f32 {
+        self.z
+    }
+
+    pub fn length(&self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    pub fn squared_length(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn make_unit(&mut self) {
+        let l = self.length();
+        debug_assert!(l != 0.0, "Division by 0.");
+        let k = 1.0 / l;
+        self.x *= k;
+        self.y *= k;
+        self.z *= k;
+    }
+
+    pub fn dot(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn cross(&self, rhs: &Self) -> Self {
+        Vec3::new(
+            self.y * rhs.z - self.z * rhs.y,
+            self.z * rhs.x - self.x * rhs.z,
+            self.x * rhs.y - self.y * rhs.x,
+        )
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z);
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self {
+        debug_assert!(rhs.x != 0.0, "Division by 0.");
+        debug_assert!(rhs.y != 0.0, "Division by 0.");
+        debug_assert!(rhs.z != 0.0, "Division by 0.");
+        Vec3::new(self.x / rhs.x, self.y / rhs.y, self.z / rhs.z)
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self {
+        debug_assert!(rhs != 0.0, "Division by 0.");
+        Vec3::new(self.x / rhs, self.y / rhs, self.z / rhs)
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, rhs: Self) {
+        debug_assert!(rhs.x != 0.0, "Division by 0.");
+        debug_assert!(rhs.y != 0.0, "Division by 0.");
+        debug_assert!(rhs.z != 0.0, "Division by 0.");
+        *self = Vec3::new(self.x / rhs.x, self.y / rhs.y, self.z / rhs.z);
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, rhs: f32) {
+        debug_assert!(rhs != 0.0, "Division by 0.");
+        let k = 1.0 / rhs;
+        *self = Vec3::new(self.x * k, self.y * k, self.z * k);
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self {
+        Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3::new(self * rhs.x, self * rhs.y, self * rhs.z)
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z);
+    }
+}
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        *self = Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs);
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3::new(-self.x, -self.y, -self.z)
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        debug_assert!(index < 3, "Index out of range.");
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        debug_assert!(index < 3, "Index out of range.");
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
+use raytracer::Vec3;
+
 fn main() {
     let nx = 200;
     let ny = 100;
     print!("P3\n{} {}\n255\n", nx, ny);
     for y in (0..ny).rev() {
         for x in 0..nx {
-            let r = x as f32 / nx as f32;
-            let g = y as f32 / ny as f32;
-            let b = 0.2;
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let col = Vec3::new(x as f32 / nx as f32, y as f32 / ny as f32, 0.2);
+            let ir = (255.99 * col[0]) as i32;
+            let ig = (255.99 * col[1]) as i32;
+            let ib = (255.99 * col[2]) as i32;
             print!("{} {} {}\n", ir, ig, ib);
         }
     }
