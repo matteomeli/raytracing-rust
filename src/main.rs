683eec78c2f3ccf36481c0ae06794b9f32a4f574
Matteo Meli <matteo.meli@gmail.com>
2021-04-25 15:16:35 +0100

    print image to stdout using PPM file format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, ray tracing world!");
+    let height = 256;
+    let width = 256;
+
+    println!("P3\n{} {}\n255", width, height);
+
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let r = i as f64 / (width - 1) as f64;
+            let g = j as f64 / (height - 1) as f64;
+            let b = 0.25;
+
+            let r = (r * 255.999) as i32;
+            let g = (g * 255.999) as i32;
+            let b = (b * 255.999) as i32;
+
+            println!("{} {} {}", r, g, b);
+        }
+    }
 }
