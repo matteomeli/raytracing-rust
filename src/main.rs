d08985ae82cfd8c1847c867ebe45d7a3c6ef67ba
Matteo Meli <matteo.meli@gmail.com>
2021-05-23 16:44:21 +0100

    send rays into scene

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,52 @@ use std::{
     thread,
 };
 
+use raymond::{Color, Point3, Ray, Vec3};
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction.to_unit();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t) * Color::white() + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
-    let height = 256;
-    let width = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i64;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+    let origin = Point3::zero();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Print completion progress to standard error
     let mut stderr = io::stderr();
 
-    println!("P3\n{} {}\n255", width, height);
+    println!("P3\n{} {}\n255", image_width, image_height);
 
-    for j in (0..height).rev() {
-        let progress = (height - j) as f32 / height as f32 * 100.0;
+    for j in (0..image_height).rev() {
+        let progress = (image_height - j) as f32 / image_height as f32 * 100.0;
         // Could use eprintln!() macro here to write to sterr,
         // but carriage return '\r' doesn't seem to work with it.
         stderr
             .write(format!("\rCompletion: {:.1}%", progress).as_bytes())
             .unwrap();
 
-        for i in 0..width {
-            let r = i as f64 / (width - 1) as f64;
-            let g = j as f64 / (height - 1) as f64;
-            let b = 0.25;
-
-            let r = (r * 255.999) as i32;
-            let g = (g * 255.999) as i32;
-            let b = (b * 255.999) as i32;
-
-            println!("{} {} {}", r, g, b);
+        for i in 0..image_width {
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let ray = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let color = ray_color(&ray);
+            println!("{}", color);
         }
 
         // Add small sleep to make completion progress actually show up ;)
